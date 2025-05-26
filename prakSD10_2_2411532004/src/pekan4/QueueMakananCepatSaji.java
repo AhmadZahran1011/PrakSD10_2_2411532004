657209054dbd057e6b2d24a78a1637b1ff4ec20a
Ahmad Zahran <Ahmad Zahran@LAPTOP-5D28A1OS>
2025-05-26 09:25:02 +0700

    tugas pekan 4

---
+++ prakSD10_2_2411532004/src/pekan4/QueueMakananCepatSaji.java
@@ -0,0 +1,41 @@
+package pekan4;
+
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Pelanggan {
+    String id;
+    int jumlahPesanan;
+
+    Pelanggan(String id, int jumlahPesanan) {
+        this.id = id;
+        this.jumlahPesanan = jumlahPesanan;
+    }
+}
+
+public class QueueMakananCepatSaji {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Queue<Pelanggan> antrian = new LinkedList<>();
+
+        System.out.print("Masukkan jumlah pelanggan: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String id = scanner.next();
+            int jumlahPesanan = scanner.nextInt();
+            antrian.add(new Pelanggan(id, jumlahPesanan));
+        }
+
+        int waktuTotal = 0;
+        while (!antrian.isEmpty()) {
+            Pelanggan p = antrian.remove();
+            waktuTotal += p.jumlahPesanan;
+            System.out.println(p.id + " selesai dalam " + waktuTotal + " menit");
+        }
+
+        scanner.close();
+    }
+}
